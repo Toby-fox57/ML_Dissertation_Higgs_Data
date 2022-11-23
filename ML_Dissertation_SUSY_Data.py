2c7c29b8b7dd6e81d223ad0438c37bbb21b846b7
Toby Fox <toby-fox2001@outlook.com>
2022-11-23 15:23:03 +0000

    Added docstrings.

--- ML_Dissertation_SUSY_Data.py
+++ ML_Dissertation_SUSY_Data.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
@@ -12,121 +11,166 @@ num_rows = 100000  # Defines the amount of collision data used.
 low, high = [np.arange(1, 9), "low"], [np.arange(9, 19), "high"]  # Defines the columns which have high-level and
 # low-level data.
 data_level, is_deep = low, 1  # Set whether to use high or low level data and if to use a deep or shallow NN.
-num_components = 0
+num_components = 0  # Sets the number of principal components used, 0 = original data.
 
 
 def test_and_train(data_level, num_rows, file_name):
-    data = pd.read_csv(file_name, nrows=num_rows, usecols=data_level)  # Reads the data from the csv.
-    label = pd.read_csv(file_name, nrows=num_rows, usecols=[0]).astype(int)  # Reads the label from the csv.
+    """
+    Read the csv and stores all the data, splitting it in data and labels.
+    Converts the pandas dataframe to a numpy array and then normalises the data between 0 and 1
+    Splits the data into train (60%), testing (20%) and evaluation (20%).
+    :param data_level: Array containing the column indexes of either low or high data
+    :param num_rows: Integer of how many rows are to be read
+    :param file_name: String containing the file location
+    :return: Arrays containing the data and labels that are used to train, test and validation the model
+    """
+    data = pd.read_csv(file_name, nrows=num_rows, usecols=data_level)
+    label = pd.read_csv(file_name, nrows=num_rows, usecols=[0]).astype(int)
 
-    data, label = data.to_numpy(), label.to_numpy()  # Converts from a dataframe to a numpy array.
+    data, label = data.to_numpy(), label.to_numpy()
+    data = keras.utils.normalize(data, axis=1)
 
-    x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.1)  # splits the data into test
-    # and train, with 90% of the data used to train and 10% used to test.
+    x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.4)
+    x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.5)
 
-    return x_train, x_test, y_train, y_test
+    return x_train, x_test, x_val, y_train, y_test, y_val
 
 
-def principle_component_analysis(train, test, num_components):
+def principle_component_analysis(train, test, val, num_components):
+    """
+    Changes the dimensions of the array from the input dimensions to the amount of principal components.
+    :param train: Array of the train set with the input dimensions.
+    :param test: Array of the test set with the input dimensions.
+    :param val: Array of the validation set with the input dimensions.
+    :param num_components: Integer of the amount of dimensions the principal component array has.
+    :return: Arrays of the training, testing and validation data with dimensions equal to the principal components.
+    """
     if num_components > 0:
         pca = PCA(n_components=num_components)
-        pca.fit(train), pca.fit(test)
+        pca.fit(train), pca.fit(test), pca.fit(val)
 
-        pca_train, pca_test = pca.transform(train), pca.transform(test)
+        pca_train, pca_test, pca_val = pca.transform(train), pca.transform(test), pca.transform(val)
 
-        return pca_train, pca_test
+        return pca_train, pca_test, pca_val
 
     else:
-        return train, test
+        return train, test, val
 
 
 def build_model(input_dim, is_deep):
+    """
+    Builds the model, it is built with either 1 hidden layer, for a shallow network, or 5 hidden layers, for a deep
+    network.
+    Each layer has an initial weight which is random value from a normal distribution.
+    The model is built up of,
+    An input layer which has 300 units and dimensions equal to the dimensions of the data, activation of tanh is used.
+    Hidden layer/s which has 300 units and an activation of tanh.
+    An output layer which has 1 unit and uses an activation of a sigmoid function, as the output is either 1 or 0.
+    :param input_dim: Integer of the dimensions of the data.
+    :param is_deep: Integer of 1 or 0, 1 for a deep network or 0 for a shallow network.
+    :return: The model.
+    """
     if is_deep == 0:
-        n = 1  # Sets the number of hidden layers to 1 if it is a shallow NN.
+        n = 1  # Shallow.
 
     else:
-        n = 5  # Sets the number of hidden layers to 5 if it is a deep NN.
+        n = 5  # Deep.
 
-    input_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.1, seed=None)  # The weights for the input
-    # layer were initialized using a random normal distribution with a mean of 0 and std of 0.1.
-    hidden_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.05, seed=None)  # The weights for the input
-    # layer were initialized using a random normal distribution with a mean of 0 and std of 0.05.
-    output_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.001, seed=None)  # The weights for the input
-    # layer were initialized using a random normal distribution with a mean of 0 and std of 0.001.
+    input_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.1, seed=None)
+    hidden_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.05, seed=None)
+    output_initializer = keras.initializers.RandomNormal(mean=0., stddev=0.001, seed=None)
 
     model = keras.Sequential()
-    model.add(
-        keras.layers.Dense(300, input_dim=input_dim, activation='tanh', kernel_initializer=input_initializer))
-    # Creates the input layer which has 300 units, the input dimension is the number of columns of data
-    # An activation function of tanh is used as it is zero centred meaning it will have a strong gradient which works
-    # well will a binary result.
+    model.add(keras.layers.Dense(300, input_dim=input_dim, activation='tanh', kernel_initializer=input_initializer))
 
     for i in range(n):
         model.add(
-            keras.layers.Dense(300, activation='tanh', kernel_initializer=hidden_initializer))  # Creates a hidden layer
-        # containing 300 units and uses an activation function of tanh.
+            keras.layers.Dense(300, activation='tanh', kernel_initializer=hidden_initializer))
 
-    model.add(
-        keras.layers.Dense(1, activation='sigmoid', kernel_initializer=output_initializer))  # Output layer, has 1 unit
-    #  as the result is binary answer, uses a sigmoid function as the answer is 0 or 1, so it cannot be zero centred.
+    model.add(keras.layers.Dense(1, activation='sigmoid', kernel_initializer=output_initializer))
 
     return model
 
 
 def compiler(model):
-    sgd = keras.optimizers.SGD(learning_rate=0.05, decay=1e-5, momentum=0.9)  # Defines the gradient descent again
-    # taken from the report.
+    """
+    Compiles the model, a Stochastic gradient descent is used with rates given from the script, the loss is using
+    a binary cross entropy as this is a binary classification and the metric of accuracy is chosen to measure the model.
+    :param model: The model.
+    :return: The compiled model.
+    """
+    sgd = keras.optimizers.SGD(learning_rate=0.05, decay=1e-5, momentum=0.9)
 
-    model.compile(loss='binary_crossentropy',  # Uses binary cross entropy as the results is a binary answer.
-                  optimizer=sgd,  # Uses the gradient descent as defined.
-                  metrics=['accuracy'])  # Measures the model on its accuracy.
+    model.compile(loss='binary_crossentropy',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
 
     return model
 
 
-def scheduler(epoch, current_learning_rate):
-    if current_learning_rate > 1e-6:  # Define a learning rate schedule, such that the learning rate decays by
-        return current_learning_rate / 1.0000002  # a factor of 1.0000002 until the learning rate reaches 1e-6.
+def model_fit_test(model, model_name, x_train, x_test, x_val, y_train, y_test, y_val):
+    """
+    The fits and evaluates the model. The fitting uses batches of 100 and fits over 1000 epochs, callbacks are used
+    to implement and early stop, so the model does not over fit and to log all the training into a csv file.
+    :param model: The model
+    :param model_name: String determine the "model name", whether it is a shallow/deep network, using high/low level
+    data and if it uses pca.
+    :param x_train, x_test, x_val: Array containing the data for the training, testing and validation set.
+    :param  y_train, y_test, y_val: Array containing the labels for the training, testing and validation set.
+    """
+    early_stop = keras.callbacks.EarlyStopping(monitor="loss", min_delta=0.00001, patience=10, verbose=1)
+    csv_log = keras.callbacks.CSVLogger(model_name, separator=",", append=False)
+
+    model.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=100, epochs=1000,
+              callbacks=[early_stop, csv_log])
+    model.evaluate(x_test, y_test)
+
+    return
+
+
+def print_statement(data_level, is_deep, num_components):
+    """
+    Prints the model use and creates the model name.
+    :param data_level: Array containing the string which says whether the network is shallow or deep.
+    :param is_deep: Integer of 1 or 0, 1 for deep and 0 for shallow.
+    :param num_components: Integer determining the number of components for the pca.
+    :return: String of the model name.
+    """
+    learning = ["shallow", "deep"]
+    print("Running:", learning[is_deep], "learning for", data_level[1], "-level data.")
+
+    if num_components > 0:
+        print("Using pca with,", num_components, "components.")
+        model_name = str("History/" + learning[is_deep] + "_" + data_level[1] + "comp" + num_components + ".csv")
 
     else:
-        return 1e-6
+        model_name = str("History/" + learning[is_deep] + "_" + data_level[1] + ".csv")
 
-
-def model_fit_test(model, x_train, x_test, y_train, y_test):
-    lr_schedule = tf.keras.callbacks.LearningRateScheduler(scheduler)
-    early_stop = keras.callbacks.EarlyStopping(monitor="loss", min_delta=0.00001, patience=10, verbose=1)  # Steps
-    # criteria to stop early, when the change in the loss over 10 epochs is less than 0.00001 the code will stop early
-    # the is done to stop over-fitting.
-
-    model.fit(x_train, y_train, batch_size=100, epochs=1000, callbacks=[lr_schedule, early_stop])  # fits the model
-    # using the training data.
-    model.evaluate(x_test, y_test)  # evaluates the trained model against the test data.
+    return model_name
 
 
 def main(file_dir, data_level, is_deep, num_components):
-    x_train, x_test, y_train, y_test = test_and_train(data_level[0], num_rows, file_dir)
-    x_train, x_test = principle_component_analysis(x_train, x_test, num_components)
+    """
+    Get the training, testing and validation data.
+    Converts the dimensions of the data to the numver of components if pca is used.
+    Prints the model name and the builds the models based of the given parameters.
+    Compiles and then test the model.
+    :param file_dir: String containing the location of the data
+    :param data_level: Array containing the column index for the data and the name of the data used.
+    :param is_deep: Integer of 1 or 0, 1 for deep and 0 for shallow.
+    :param num_components: Integer determining the number of components for the pca.
+    :return: Integer, 0 if the code is succesful.
+    """
+    x_train, x_test, x_val, y_train, y_test, y_val = test_and_train(data_level[0], num_rows, file_dir)
+    x_train, x_test, x_val = principle_component_analysis(x_train, x_test, x_val, num_components)
+
+    model_name = print_statement(data_level, is_deep, num_components)
 
     model = build_model(np.shape(x_train)[1], is_deep)
     model = compiler(model)
-
-    learning = ["shallow", "deep"]
-    print("Running:", learning[is_deep], "learning for", data_level[1], "-level data.")
-    if num_components > 0:
-        print("Using pca with,", num_components, "components.")
-
-    model_fit_test(model, x_train, x_test, y_train, y_test)
+    model_fit_test(model, model_name, x_train, x_test, x_val, y_train, y_test, y_val)
 
     return 0
 
 
 main(file_dir, data_level, is_deep, num_components)
-
-# Shallow low-level --> training: 80.55%,  385 Epochs / testing: 78.94%
-# Shallow high-level --> training: 79.19%,   131 Epochs / testing: 78.92%
-
-# Deep low-level --> training: 99.06%, 306 Epochs / testing: 74.64% ??
-# Deep high-level --> training: 95.63%,  494 Epochs / testing: 72.87% ??
-
-# Deep low-level, 2 components --> training: 57.42%, 50 Epochs / testing: 55.16%
-# Deep high-level, 2 components --> training: 71.29%, 70 Epochs / testing: 70.77%
